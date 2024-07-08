5de325c88bfa82b59c550b550e403dac6cce4621
Bolero-fk <fuku_0000@outlook.jp>
2024-07-08 21:59:19 +0900

    バーチャルコンテストのページを開いた直後は動作しないバグを修正した

--- src/main.js
+++ src/main.js
@@ -43,9 +43,8 @@
     }
 
     function getActiveTab() {
-        const hash = window.location.hash;
-        const urlParams = new URLSearchParams(hash.split('?')[1]);
-        return urlParams.get('activeTab');
+        const activeTabElement = document.querySelector('a.nav-link.active');
+        return activeTabElement?.innerText;
     }
 
     function hasResultCell(cells) {
